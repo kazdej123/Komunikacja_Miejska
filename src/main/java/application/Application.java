326d5afb6d69f17961bad3a9d87dde6206155017
kazdej123 <kamilmurawski92@gmail.com>
2018-08-13 13:44:25 +0200

    v1.1.2 Usunięto niepotrzebny konstruktor Application().

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -10,14 +10,12 @@ final class Application {
     public static void main(final String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            EventQueue.invokeLater(Application::new);
+            EventQueue.invokeLater(() -> {
+                final View view = new DefaultView();
+                view.showLoginWindow();
+            });
         } catch (final Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private Application() {
-        final View view = new DefaultView();
-        view.showLoginWindow();
     }
 }
