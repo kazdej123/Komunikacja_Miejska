f0476d1f6f317cd3909c2a9830439f704bb18c0d
kazdej123 <kamilmurawski92@gmail.com>
2018-08-14 01:18:21 +0200

    v1.1.3 Poprawiono kod.

--- src/main/java/view/DefaultView.java
+++ src/main/java/view/DefaultView.java
@@ -1,6 +1,7 @@
 package view;
 
 import controller.Controller;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.BorderFactory;
@@ -24,9 +25,12 @@ import javax.swing.WindowConstants;
 import javax.swing.table.AbstractTableModel;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Frame;
+import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.event.ActionListener;
@@ -35,23 +39,23 @@ import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.awt.Component.CENTER_ALIGNMENT;
-
 public final class DefaultView implements View {
+    private static final String FONT_NAME = "Tahoma";
+
     @Override
     public final void showLoginWindow() {
         final JDialog dialog = new JDialog((Frame) null, "Okno logowania", true);
 
         final Controller controller = null;
 
-        addWindowListener(dialog, controller);
+        addWindowClosingListener(dialog, controller);
 
         final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
         final int screenWidth = screenSize.width;
         final int screenHeight = screenSize.height;
 
-        final JButton loginButton = createJButton("Zaloguj", e -> {
+        final JButton loginButton = createButton("Zaloguj", e -> {
             if (controller != null) {
                 controller.login();
             } else {
@@ -65,15 +69,15 @@ public final class DefaultView implements View {
 
                 final List<JPanel> mainPanelsList = new ArrayList<>();
 
-                final JPanel mainPanel = new JPanel(new BorderLayout(), true);
+                final JPanel mainPanel = createDoubleBufferedPanel(new BorderLayout());
 
                 final String string = "Gminy";
 
                 mainPanel.setName(string);
 
-                final JPanel cardsPanel = new JPanel(new CardLayout(), true);
+                final JPanel cardsPanel = createDoubleBufferedPanel(new CardLayout());
 
-                final JPanel tablePanel = new JPanel(new BorderLayout(), true);
+                final JPanel tablePanel = createDoubleBufferedPanel(new BorderLayout());
 
                 final String ALL = "All";
 
@@ -99,14 +103,14 @@ public final class DefaultView implements View {
                 table.setAutoCreateRowSorter(true);
 
                 final JScrollPane scrollPane = new JScrollPane(table);
-                setBorder(scrollPane, 20, 10);
+                setEmptyBorder(scrollPane, 20, 10, 10, 10);
 
                 tablePanel.add(scrollPane, BorderLayout.CENTER);
 
-                final JPanel internalButtonsPanel = new JPanel(true);
-                internalButtonsPanel.add(createJButton("Dodaj rekord", null));
-                internalButtonsPanel.add(createJButton("Usuń rekordy", null));
-                setBorder(internalButtonsPanel, 10, 80);
+                final JPanel internalButtonsPanel = createDoubleBufferedPanel(new FlowLayout());
+                internalButtonsPanel.add(createAdjustedButton("Dodaj rekord", null));
+                internalButtonsPanel.add(createAdjustedButton("Usuń rekordy", null));
+                setEmptyBorder(internalButtonsPanel, 10, 10, 80, 10);
 
                 tablePanel.add(internalButtonsPanel, BorderLayout.SOUTH);
 
@@ -114,26 +118,25 @@ public final class DefaultView implements View {
 
                 mainPanel.add(cardsPanel, BorderLayout.CENTER);
 
-                final JPanel buttonsPanel = new JPanel(true);
+                final JPanel buttonsPanel = createDoubleBufferedPanel(null);
                 buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
 
                 final Dimension dimension = new Dimension(270, 50);
 
-                buttonsPanel.add(createJLabel("Wyświetl " + string.toLowerCase(), dimension));
+                buttonsPanel.add(createLabel("Wyświetl " + string.toLowerCase(), dimension));
                 buttonsPanel.add(Box.createRigidArea(new Dimension(0, 15)));
 
-                final JButton button = createJButton("wszystkie", e1 -> {
+                final JButton button = createAdjustedButton("wszystkie", e1 -> {
                     if (controller != null) {
                         controller.showGminy();
                     } else {
                         ((CardLayout)cardsPanel.getLayout()).show(cardsPanel, ALL);
                     }
                 });
-                setJComponentProperties(button, Font.PLAIN, dimension);
-                button.setHorizontalAlignment(SwingConstants.CENTER);
+                setDefaultJComponentProperties(button, dimension);
 
                 buttonsPanel.add(button);
-                buttonsPanel.setBorder(BorderFactory.createEmptyBorder(30, 20,30, 50));
+                setEmptyBorder(buttonsPanel, 30, 20, 30, 50);
 
                 mainPanel.add(buttonsPanel, BorderLayout.EAST);
 
@@ -141,10 +144,10 @@ public final class DefaultView implements View {
 
                 for (int i = 0; i < mainPanelsList.size(); i++) {
                     tabbedPane.add(mainPanelsList.get(i));
-                    tabbedPane.setTabComponentAt(i, createJLabel(tabbedPane.getTitleAt(i), new Dimension(180, 50)));
+                    tabbedPane.setTabComponentAt(i, createLabel(tabbedPane.getTitleAt(i), new Dimension(180, 50)));
                 }
                 frame.add(tabbedPane);
-                addWindowListener(frame, controller);
+                addWindowClosingListener(frame, controller);
                 frame.setSize(screenWidth, screenHeight);
                 frame.setExtendedState(Frame.MAXIMIZED_BOTH);
                 frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
@@ -161,10 +164,10 @@ public final class DefaultView implements View {
         final JTextField usernameField = new JTextField("", 20);
         final JPasswordField passwordField = new JPasswordField("", 20);
 
-        final JButton cancelButton = createJButton("Anuluj", e -> closeWindow(controller, dialog));
+        final JButton cancelButton = createButton("Anuluj", e -> closeWindow(dialog, controller));
 
         groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.CENTER).addGroup(groupLayout.createSequentialGroup().addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.TRAILING).addComponent(usernameLabel).addComponent(passwordLabel)).addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(usernameField).addComponent(passwordField))).addGroup(groupLayout.createSequentialGroup().addComponent(loginButton).addComponent(cancelButton)));
-        groupLayout.setVerticalGroup(groupLayout.createSequentialGroup().addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(usernameLabel).addComponent(usernameField)).addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(passwordLabel).addComponent(passwordField)).addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(loginButton).addComponent(cancelButton)));
+        groupLayout.setVerticalGroup(groupLayout.createSequentialGroup().addGroup(createBaselineGroup(groupLayout).addComponent(usernameLabel).addComponent(usernameField)).addGroup(createBaselineGroup(groupLayout).addComponent(passwordLabel).addComponent(passwordField)).addGroup(createBaselineGroup(groupLayout).addComponent(loginButton).addComponent(cancelButton)));
 
         dialog.setLayout(groupLayout);
         dialog.pack();
@@ -179,16 +182,16 @@ public final class DefaultView implements View {
         dialog.setVisible(true);
     }
 
-    private static void addWindowListener(@NotNull final Window window, final Controller controller) {
+    private static void addWindowClosingListener(@NotNull final Window window, final Controller controller) {
         window.addWindowListener(new WindowAdapter() {
             @Override
             public final void windowClosing(final WindowEvent e) {
-                closeWindow(controller, window);
+                closeWindow(window, controller);
             }
         });
     }
 
-    private static void closeWindow(final Controller controller, final Window window) {
+    private static void closeWindow(final Window window, final Controller controller) {
         if (controller != null) {
             controller.exit();
         } else {
@@ -196,25 +199,41 @@ public final class DefaultView implements View {
         }
     }
 
-    private static JButton createJButton(final String text, final ActionListener actionListener) {
+    @NotNull
+    @Contract("_ -> new")
+    private static JPanel createDoubleBufferedPanel(final LayoutManager layout) {
+        return new JPanel(layout, true);
+    }
+
+    private static void setEmptyBorder(@NotNull final JComponent component, final int top, final int left, final int bottom, final int right) {
+        component.setBorder(BorderFactory.createEmptyBorder(top, left, bottom, right));
+    }
+
+    private static JButton createAdjustedButton(final String text, final ActionListener actionListener) {
+        final JButton button = createButton(text, actionListener);
+        button.setFont(new Font(FONT_NAME, Font.PLAIN, 20));
+        return button;
+    }
+
+    private static JButton createButton(final String text, final ActionListener actionListener) {
         final JButton button = new JButton(text);
         button.addActionListener(actionListener);
         return button;
     }
 
-    private static void setBorder(@NotNull final JComponent component, final int top, final int bottom) {
-        component.setBorder(BorderFactory.createEmptyBorder(top, 10, bottom, 10));
-    }
-
-    private static JLabel createJLabel(final String text, final Dimension dimension) {
+    private static JLabel createLabel(final String text, final Dimension dimension) {
         final JLabel label = new JLabel(text, SwingConstants.CENTER);
-        setJComponentProperties(label, Font.BOLD, dimension);
+        setDefaultJComponentProperties(label, dimension);
+        label.setFont(new Font(FONT_NAME, Font.BOLD, 20));
         return label;
     }
 
-    private static void setJComponentProperties(@NotNull final JComponent component, final int fontStyle, final Dimension dimension) {
-        component.setAlignmentX(CENTER_ALIGNMENT);
-        component.setFont(new Font("Tahoma", fontStyle, 20));
+    private static void setDefaultJComponentProperties(@NotNull final JComponent component, final Dimension dimension) {
+        component.setAlignmentX(Component.CENTER_ALIGNMENT);
         component.setPreferredSize(dimension);
+    }
+
+    private static GroupLayout.ParallelGroup createBaselineGroup(@NotNull final GroupLayout groupLayout) {
+        return groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE);
     }
 }
