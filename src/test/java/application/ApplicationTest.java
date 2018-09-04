3ba15e489b481ca6411b75d6736e212465e57fb5
kazdej123 <kamilmurawski92@gmail.com>
2018-09-04 17:46:34 +0200

    1.5.0 Dodano test dla metody main().

--- src/test/java/application/ApplicationTest.java
+++ src/test/java/application/ApplicationTest.java
@@ -1,20 +1,10 @@
 package application;
 
 import org.junit.Test;
-import view.DefaultView;
-
-import javax.swing.UIManager;
-import java.awt.EventQueue;
 
 public final class ApplicationTest {
     @Test
-    public final void mainTest1() {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            EventQueue.invokeLater(DefaultView::new);
-        } catch (final Exception e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
+    public void main() {
+        Application.main(null);
     }
 }
